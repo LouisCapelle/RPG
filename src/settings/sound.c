bf9c95348e11b358d695075de80882526e41280d
louiscapelle <louis.capelle@epitech.eu>
2020-05-03 11:37:27 +0200

    sound bar is now working

--- src/settings/sound.c
+++ src/settings/sound.c
@@ -15,13 +15,15 @@ int get_value_sound(game_t *game)
     if (!game)
         return 84;
     result = pos.x - 800;
-    return 0;
+    return result;
 }
 
 int modify_sound(game_t *game)
 {
-    int sound = get_value_sound(game);
+    float sound = get_value_sound(game);
 
+    if (sound >= 9 && sound <= 100)
+        sfMusic_setVolume(game->utils->back_music, sound);
     if (!game)
         return 84;
     return 0;
